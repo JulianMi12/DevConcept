32fb9633769cab92bd4fe3a6f4082325204d8cd4
Daniela <daniela.saganomeg@konradlorenz.edu.co>
2021-10-24 11:42:17 -0500

    Desarrollo del area service

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { CargarScriptsService } from 'app/services/cargar-scripts.service';
+
 
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css']
+  styleUrls: ['./service.component.css', './style2.css']
 })
 export class ServiceComponent implements OnInit {
 
